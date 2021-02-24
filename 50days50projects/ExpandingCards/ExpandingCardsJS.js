1f80021ce55106a3c93f49ea792793249138dd5a
Dilan Bautista <dilan_bautista@ymail.com>
2021-02-24 17:36:01 -0500

    Added a new Menu technique called expanding cards

---
+++ 50days50projects/ExpandingCards/ExpandingCardsJS.js
@@ -0,0 +1,15 @@
+const panels = document.querySelectorAll('.panel')
+// Gather an array of data pertaining to all panels
+
+panels.forEach((panel) => {
+    panel.addEventListener('mouseover',() => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses(){
+    panels.forEach((panel) => {
+          panel.classList.remove('active')
+    })
+}
