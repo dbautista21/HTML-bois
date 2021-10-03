ef4bca2b9bbf08748ddff2cdd1d35883bfac0df8
Dilan Bautista <dilan_bautista@ymail.com>
2021-10-02 22:04:33 -0400

    Recalibrate Repo

--- 50days50projects/ExpandingCards/ExpandingCardsJS.js
+++ 50days50projects/ExpandingCards/ExpandingCardsJS.js
@@ -1,15 +1,15 @@
-const panels = document.querySelectorAll('.panel')
+const panels = document.querySelectorAll(".panel");
 // Gather an array of data pertaining to all panels
 
 panels.forEach((panel) => {
-    panel.addEventListener('mouseover',() => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+  panel.addEventListener("mouseover", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
 
-function removeActiveClasses(){
-    panels.forEach((panel) => {
-          panel.classList.remove('active')
-    })
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
 }
